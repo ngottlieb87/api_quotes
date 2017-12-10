123281bd38eac01f00b33280593040e64ef0687e
ngottlieb87 <nogpdx@gmail.com>
2017-12-10 15:40:53 -0800

    api routes and testing

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,35 @@
+class QuotesController < ApplicationController
+
+  def index
+    @quotes = Quote.all
+    json_response(@quotes)
+  end
+
+  def show
+    @quote = Quote.find(params[:id])
+    json_response(@quote)
+  end
+
+  def create
+    @quote = Quote.create!(quote_params)
+    json_response(@quote)
+  end
+
+  def update
+    if @quote.update!(quote_params)
+      render status: 200, json: {
+       message: "Your quote has been updated successfully."
+       }
+    end
+  end
+
+  def destroy
+    @quote = Quote.find(params[:id])
+    @quote.destroy
+  end
+
+  private
+  def quote_params
+    params.permit(:author, :content)
+  end
+end
---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+
+  factory(:quote) do
+    content("I will choke you with my big toe!")
+    author("Chuck Norris")
+  end
+end
